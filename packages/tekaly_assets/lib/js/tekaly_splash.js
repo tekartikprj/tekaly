bbb090cf0885f9877321c41959707e3b3d1f7674
Alexandre Roux <alex@tekartik.com>
2024-07-24 12:46:29 +0200

    [tekaly_assets] fix splash js to be safe

--- packages/tekaly_assets/lib/js/tekaly_splash.js
+++ packages/tekaly_assets/lib/js/tekaly_splash.js
@@ -1,34 +1,33 @@
-document.write(`
-<style>
-    body {
-        /* background-color: red; */
-    }
+const style = document.createElement("style")
+style.textContent = `
+#app_splash {
+    padding: 32px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    opacity: 0;
+    transition: opacity 300ms cubic-bezier(0, 0, 0.2, 1);
+    will-change: opacity;
+    z-index: 100;
+    background: black url("assets/packages/tekaly_assets/img/tekartik_logo_256.png") no-repeat center;
+    background-size: 256px 256px;
+    background-origin: content-box;
+}
 
+#app_splash.app-loading {
+    opacity: 1;
+}
+
+@media screen and (max-width: 384px) {
     #app_splash {
-        padding: 32px;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        opacity: 0;
-        transition: opacity 300ms cubic-bezier(0, 0, 0.2, 1);
-        will-change: opacity;
-        z-index: 100;
-        background: black url("assets/packages/tekaly_assets/img/tekartik_logo_256.png") no-repeat center;
-        background-size: 256px 256px;
-        background-origin: content-box;
+        background-size: contain;
     }
-
-    #app_splash.app-loading {
-        opacity: 1;
-    }
-
-    @media screen and (max-width: 384px) {
-        #app_splash {
-            background-size: contain;
-        }
-    }
-</style>
-<div id="app_splash" class="app-loading"></div>
-`);
+}
+`;
+document.head.appendChild(style)
+const div = document.createElement("div")
+div.id = "app_splash"
+div.className = "app-loading"
+document.body.appendChild(div)
