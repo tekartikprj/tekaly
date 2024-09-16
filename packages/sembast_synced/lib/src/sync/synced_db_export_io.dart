c6ccfe84521cd032ca08b275357f39b482f074bd
Alexandre Roux <alex@tekartik.com>
2024-09-16 12:13:08 +0200

    fix import

--- packages/sembast_synced/lib/src/sync/synced_db_export_io.dart
+++ packages/sembast_synced/lib/src/sync/synced_db_export_io.dart
@@ -6,6 +6,7 @@ import 'package:sembast/utils/sembast_import_export.dart';
 import 'package:tekaly_sembast_synced/src/sync/synced_db.dart';
 import 'package:tekaly_sembast_synced/src/sync/synced_db_export.dart';
 import 'package:tekaly_sembast_synced/src/sync/synced_source_export.dart';
+import 'package:tekartik_app_cv_sembast/app_cv_sembast.dart';
 
 import 'import_common.dart';
 
