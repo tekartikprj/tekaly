2fc66bab3411317aa935aabbea29b827cfdc9c85
Alexandre Roux <alex@tekartik.com>
2025-06-22 15:47:56 +0200

    fix download image io

--- flutter_packages/file_download/lib/src/download/download_image_io.dart
+++ flutter_packages/file_download/lib/src/download/download_image_io.dart
@@ -1,17 +1,10 @@
-import 'dart:io';
-
 import 'package:file_picker/file_picker.dart';
 
 import 'download_image.dart';
 
 Future<void> downloadImage(DownloadImageInfo imageInfo) async {
-  var outputFile = await FilePicker.platform.saveFile(
-    //dialogTitle: 'Please select an output file:',
+  await FilePicker.platform.saveFile(
+    bytes: imageInfo.data,
     fileName: imageInfo.filename,
   );
-
-  if (outputFile != null) {
-    await File(outputFile).writeAsBytes(imageInfo.data);
-    // User canceled the picker
-  }
 }
