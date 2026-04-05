422c3ef2b8b602acae6940eadcc666cdc1534795
Alexandre Roux <alex@tekartik.com>
2026-04-05 12:29:58 +0200

    fix lints

--- flutter_packages/file_download/lib/src/download/download_image_io.dart
+++ flutter_packages/file_download/lib/src/download/download_image_io.dart
@@ -3,7 +3,7 @@ import 'package:file_picker/file_picker.dart';
 import 'download_image.dart';
 
 Future<void> downloadImage(DownloadImageInfo imageInfo) async {
-  await FilePicker.platform.saveFile(
+  await FilePicker.saveFile(
     bytes: imageInfo.data,
     fileName: imageInfo.filename,
   );
